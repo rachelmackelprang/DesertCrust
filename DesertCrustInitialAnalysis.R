39f8a1735da17dfec910a92cf173881838fe583d
Mackelprang <rmackelpr@rachel1-eh2207.local>
2019-09-05 16:15:58 -0700

    update

--- DesertCrustInitialAnalysis.R
+++ DesertCrustInitialAnalysis.R
@@ -10,6 +10,10 @@ library("tidyverse")
 library(RColorBrewer)
 library(phyloseq)
 
+#some ggplot2 theming
+theme_set(theme_bw())
+
+
 #Import metadata
 meta<-read_tsv(file="~/Dropbox/DesertSoils/DesertSoilMetadata.txt")
 
@@ -18,22 +22,27 @@ meta_trimmed<-select(des_soil_meta, 'Seq Project Name', 'Collection Month', 'Sam
 
 #change anything of type 'chr' to a factor. Make the first column into row names
 meta_trimmed<-meta_trimmed %>% as.data.frame() %>% mutate_if(sapply(meta_trimmed, is.character), as.factor) %>% column_to_rownames('Seq Project Name')
+SDATA<-sample_data(meta_trimmed)
 
-#import KEGG data. Counts and proportions
+#import KEGG count data. 
 kegg_counts<-read_tsv(file="~/Dropbox/DesertSoils/desert_soil_nomoss_keggcount_all.txt")
-#kegg_counts<-read.table(file="~/Dropbox/DesertSoils/desert_soil_nomoss_keggcount_all.txt", header=TRUE, row.names=1, check.names=F)
-#des_kegg_prop<-read.table(file="~/Dropbox/DesertSoils/desert_soil_nomoss_keggprop_all.txt", header=TRUE, row.names=1, check.names=F)
 
 
 #create 'OTU' table using phyloseq
 kegg_counts<-kegg_counts %>% as.data.frame() %>% column_to_rownames("KEGG")
-kegg_phyloseq<-otu_table(kegg_counts, taxa_are_rows = TRUE)
+OTU<-otu_table(kegg_counts, taxa_are_rows = TRUE)
 
-physeq<-phyloseq(kegg_phyloseq, meta_trimmed)
+physeq<-phyloseq(OTU, SDATA)
 
 #prune low abundance genes (observed more than 100 times in at least 10% of samples)
 physeq.f<-filter_taxa(physeq, function(x) sum(x<=100) > (0.01 * length(x)), prune=TRUE)
 
+#get relative abundance
+physeq.f.ra<-transform_sample_counts(physeq.f, function(x) x*100/sum(x))
 
-#extract kegg table from phyloseq object and change rows to columns and vice versa
+#extract kegg relative abundance table from phyloseq object and change rows to columns and vice versa
 propData<-as.data.frame(t(otu_table(physeq.f.ra)))
+
+#compute and plot alpha diversity (note that alpha diversity in phyloseq uses count data rather than relative abundance)
+p = plot_richness(physeq.f, x = "Sample.Isolated.From", color = "Collection.Month", measures = c("Shannon", "InvSimpson"))
+p + scale_color_discrete(name="Collection Month") + xlab("")
