36f5a14fcff6c97c251d675f4a77dbc23d2bcb5f
Mackelprang <rmackelpr@rachel1-eh2207.local>
2019-09-10 09:03:45 -0700

    update

--- DesertCrustInitialAnalysis.R
+++ DesertCrustInitialAnalysis.R
@@ -43,6 +43,47 @@ physeq.f.ra<-transform_sample_counts(physeq.f, function(x) x*100/sum(x))
 #extract kegg relative abundance table from phyloseq object and change rows to columns and vice versa
 propData<-as.data.frame(t(otu_table(physeq.f.ra)))
 
-#compute and plot alpha diversity (note that alpha diversity in phyloseq uses count data rather than relative abundance)
+#compute alpha diversity stats (note that alpha diversity in phyloseq uses count data rather than relative abundance)
+alpha<-estimate_richness(physeq.f,  measures=c("Shannon", "InvSimpson"))
+
+#plot alpha diversity figure
 p = plot_richness(physeq.f, x = "Sample.Isolated.From", color = "Collection.Month", measures = c("Shannon", "InvSimpson"))
 p + scale_color_discrete(name="Collection Month") + xlab("")
+
+
+#export alpha diversity figure to pdf
+pdf("~/Dropbox/GitHub/DesertCrust/alphadiversity_figure.pdf", useDingbats = F)
+p = plot_richness(physeq.f, x = "Sample.Isolated.From", color = "Collection.Month", measures = c("Shannon", "InvSimpson"))
+p + scale_color_discrete(name="Collection Month") + xlab("") + scale_color_discrete(name="Collection Month") + xlab("")
+dev.off()
+
+#make alpha diveristy boxplot figure
+p = plot_richness(physeq.f, x = "Sample.Isolated.From", measures = c("Shannon", "InvSimpson"))
+p + xlab("") + geom_boxplot()
+
+#export alpha diversity boxplot figure to pdf
+pdf("~/Dropbox/GitHub/DesertCrust/alphadiversity_boxplotfigure.pdf", useDingbats = F)
+p = plot_richness(physeq.f, x = "Sample.Isolated.From", measures = c("Shannon", "InvSimpson"))
+p + xlab("") + geom_boxplot()
+dev.off()
+
+#anovas and alpha diversity
+alpha_for_anova<-cbind(sample_data(physeq.f), alpha)
+shannon.anova<-aov(Shannon ~ Sample.Isolated.From, alpha_for_anova)
+invsimp.anova<-aov(InvSimpson ~ Sample.Isolated.From, alpha_for_anova)
+
+#summary(shannon.anova)
+#Df Sum Sq Mean Sq F value   Pr(>F)    
+#Sample.Isolated.From  8 0.2546 0.03183   4.597 0.000468 ***
+#Residuals            41 0.2839 0.00692                     
+#---
+#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#summary(invsimp.anova)
+#Df Sum Sq Mean Sq F value   Pr(>F)    
+#Sample.Isolated.From  8 501065   62633   4.445 0.000616 ***
+#  Residuals            41 577733   14091                     
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+
