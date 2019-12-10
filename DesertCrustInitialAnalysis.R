97cb0fc2ebfe3663db19529212d59c00ded8b13c
Mackelprang <rmackelpr@rachel1-eh2207.local>
2019-12-10 13:40:10 -0800

    updated after error in code found

--- DesertCrustInitialAnalysis.R
+++ DesertCrustInitialAnalysis.R
@@ -34,7 +34,9 @@ OTU<-otu_table(kegg_counts, taxa_are_rows = TRUE)
 physeq<-phyloseq(OTU, SDATA)
 
 #prune low abundance genes (keep only those observed more than 100 times in at least 10% of samples)
-physeq.f<-filter_taxa(physeq, function(x) sum(x<=100) > (0.01 * length(x)), prune=TRUE)
+physeq.f<-filter_taxa(physeq, function(x) sum(x>100) > (0.01 * length(x)), prune=TRUE)
+
+#function(x) sum(x>100) > (0.1 * length(x))
 
 #get relative abundance
 physeq.f.ra<-transform_sample_counts(physeq.f, function(x) x*100/sum(x))
@@ -53,28 +55,27 @@ adonis(propData_bray ~ CollectionMonth, data = meta_trimmed)
 
 #adonis(propData_bray ~  CrustLayer+CrustCategory + MossAssociated +CollectionMonth + SoilType, data=meta_trimmed)
 #Call:
-#  adonis(formula = propData_bray ~ CrustLayer + CrustCategory +      MossAssociated + CollectionMonth + SoilType, data = meta_trimmed) 
+  #adonis(formula = propData_bray ~ CrustLayer + CrustCategory +      MossAssociated + CollectionMonth + SoilType, data = meta_trimmed) 
 
 #Permutation: free
 #Number of permutations: 999
 
 #Terms added sequentially (first to last)
 
-#Df SumsOfSqs  MeanSqs F.Model      R2 Pr(>F)    
-#CrustLayer       2   0.14331 0.071657  2.2497 0.06196  0.047 *  
-#  CrustCategory    1   0.04569 0.045691  1.4345 0.01975  0.215    
-#MossAssociated   1   0.02447 0.024473  0.7684 0.01058  0.499    
-#CollectionMonth  1   0.04688 0.046877  1.4717 0.02027  0.218    
-#SoilType         4   0.46002 0.115006  3.6107 0.19889  0.001 ***
-#  Residuals       50   1.59256 0.031851         0.68854           
-#Total           59   2.31294                  1.00000           
+#Df SumsOfSqs   MeanSqs F.Model      R2 Pr(>F)   
+#CrustLayer       2  0.013645 0.0068226  2.9456 0.07992  0.015 * 
+#  CrustCategory    1  0.004362 0.0043619  1.8832 0.02555  0.126   
+#MossAssociated   1  0.001554 0.0015543  0.6711 0.00910  0.577   
+#CollectionMonth  1  0.003460 0.0034599  1.4937 0.02026  0.199   
+#SoilType         4  0.031902 0.0079754  3.4432 0.18685  0.002 **
+#  Residuals       50  0.115813 0.0023163         0.67832          
+#Total           59  0.170736                   1.00000          
 #---
 #  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 
-
 #interactions between collection month and other variables? 
-adonis(propData_bray ~ CollectionMonth*CrustLayer + CollectionMonth*CrustCategory + CollectionMonth * MossAssociated + CollectionMonth*SoilType,data = meta_trimmed)
+#adonis(propData_bray ~ CollectionMonth*CrustLayer + CollectionMonth*CrustCategory + CollectionMonth * MossAssociated + CollectionMonth*SoilType,data = meta_trimmed)
 #Call:
 #  adonis(formula = propData_bray ~ CollectionMonth * CrustLayer +      CollectionMonth * CrustCategory + CollectionMonth * MossAssociated +      CollectionMonth * SoilType, data = meta_trimmed) 
 
@@ -83,18 +84,18 @@ adonis(propData_bray ~ CollectionMonth*CrustLayer + CollectionMonth*CrustCategor
 
 #Terms added sequentially (first to last)
 
-#Df SumsOfSqs  MeanSqs F.Model      R2 Pr(>F)    
-#CollectionMonth                 1   0.04697 0.046974  1.7710 0.02031  0.132    
-#CrustLayer                      2   0.14476 0.072379  2.7288 0.06259  0.013 *  
-#  CrustCategory                   1   0.04500 0.045002  1.6967 0.01946  0.147    
-#MossAssociated                  1   0.02362 0.023621  0.8905 0.01021  0.444    
-#SoilType                        4   0.46002 0.115006  4.3360 0.19889  0.001 ***
-#  CollectionMonth:CrustLayer      2   0.04657 0.023284  0.8778 0.02013  0.482    
-#CollectionMonth:CrustCategory   1   0.11987 0.119874  4.5195 0.05183  0.003 ** 
-#  CollectionMonth:MossAssociated  1   0.15760 0.157596  5.9417 0.06814  0.001 ***
-#  CollectionMonth:SoilType        4   0.15453 0.038632  1.4565 0.06681  0.143    
-#Residuals                      42   1.11400 0.026524         0.48164           
-#Total                          59   2.31294                  1.00000           
+#Df SumsOfSqs   MeanSqs F.Model      R2 Pr(>F)    
+#CollectionMonth                 1  0.002923 0.0029234  1.3929 0.01712  0.216    
+#CrustLayer                      2  0.014253 0.0071263  3.3954 0.08348  0.013 *  
+#  CrustCategory                   1  0.004433 0.0044331  2.1122 0.02596  0.102    
+#MossAssociated                  1  0.001412 0.0014123  0.6729 0.00827  0.586    
+#SoilType                        4  0.031902 0.0079754  3.8000 0.18685  0.001 ***
+#  CollectionMonth:CrustLayer      2  0.002342 0.0011712  0.5581 0.01372  0.683    
+#CollectionMonth:CrustCategory   1  0.009617 0.0096170  4.5822 0.05633  0.009 ** 
+#  CollectionMonth:MossAssociated  1  0.007119 0.0071190  3.3920 0.04170  0.025 *  
+#  CollectionMonth:SoilType        4  0.008586 0.0021464  1.0227 0.05029  0.397    
+#Residuals                      42  0.088149 0.0020988         0.51629           
+#Total                          59  0.170736                   1.00000           
 #---
 #  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
@@ -107,30 +108,29 @@ adonis(propData_bray ~ CrustCategory*MossAssociated*CrustLayer, data = meta_trim
 
 #Terms added sequentially (first to last)
 
-#Df SumsOfSqs  MeanSqs F.Model      R2 Pr(>F)    
-#CrustCategory                            2   0.15643 0.078214  2.4336 0.06763  0.026 *  
-#  MossAssociated                           1   0.02446 0.024456  0.7610 0.01057  0.535    
-#CrustLayer                               1   0.03259 0.032594  1.0141 0.01409  0.366    
-#CrustCategory:MossAssociated             1   0.27435 0.274345  8.5361 0.11861  0.001 ***
-#  CrustCategory:CrustLayer                 1   0.08805 0.088049  2.7396 0.03807  0.045 *  
-#  MossAssociated:CrustLayer                1   0.07998 0.079981  2.4886 0.03458  0.052 .  
-#CrustCategory:MossAssociated:CrustLayer  1   0.01799 0.017988  0.5597 0.00778  0.656    
-#Residuals                               51   1.63910 0.032139         0.70866           
-#Total                                   59   2.31294                  1.00000           
+#Df SumsOfSqs   MeanSqs F.Model      R2 Pr(>F)    
+#CrustCategory                            2  0.015036 0.0075179  3.2104 0.08806  0.014 *  
+#  MossAssociated                           1  0.001553 0.0015527  0.6630 0.00909  0.546    
+#CrustLayer                               1  0.002973 0.0029731  1.2696 0.01741  0.265    
+#CrustCategory:MossAssociated             1  0.018856 0.0188563  8.0522 0.11044  0.001 ***
+#  CrustCategory:CrustLayer                 1  0.004315 0.0043154  1.8428 0.02528  0.127    
+#MossAssociated:CrustLayer                1  0.006930 0.0069305  2.9595 0.04059  0.034 *  
+#  CrustCategory:MossAssociated:CrustLayer  1  0.001642 0.0016420  0.7012 0.00962  0.550    
+#Residuals                               51  0.119430 0.0023418         0.69950           
+#Total                                   59  0.170736                   1.00000           
 #---
 #  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
 
 
 #ordinations
 
 ord_bray.pcoa<-ordinate(physeq.f.ra, "PCoA", "bray")
-ord_bray.nmds<-ordinate(physeq.f.ra, "NMDS", "bray")
-p=plot_ordination(physeq.f.ra, ord_bray.pcoa, color="SoilType", shape="CollectionMonth")
+ord_bray.nmds<-ordinate(physeq.f.ra, "NMDS", "bray") # no convergance for the NMDS. Don't use these data
+p=plot_ordination(physeq.f.ra, ord_bray.pcoa, color="CrustCategory", shape="CollectionMonth")
 p+geom_point(size=3, alpha=0.75) + theme(plot.title=element_text(size=16, face="bold, vjust =2"))
 
 p=plot_ordination(physeq.f.ra, ord_bray.pcoa, color="CrustCategory", shape="CollectionMonth")
-p+geom_point(size=3, alpha=0.75) + theme(plot.title=element_text(size=16, face="bold, vjust =2"))
+p+geom_point(size=3, alpha=0.75) + theme(plot.title=element_text(size=16, face="bold, vjust =2")) #don't use these data. no convergance
 
 
 
@@ -217,15 +217,15 @@ write.table(tukey.InvSimpson$SoilType, file="tukey.invsimp.csv", sep=",")
 
 
 #alpha diversity by crust category, moss associated, and crust layer
-shannon_crust.anova<-aov(Shannon ~ CrustCategory, alpha_for_anova) #p = 0.00425
-invsimp_crust.anova<-aov(InvSimpson ~ CrustCategory, alpha_for_anova) #p=0.00397
+shannon_crust.anova<-aov(Shannon ~ CrustCategory, alpha_for_anova) # not significant
+invsimp_crust.anova<-aov(InvSimpson ~ CrustCategory, alpha_for_anova) #p=0.00942
 
 
 shannon_moss.anova<-aov(Shannon ~ MossAssociated, alpha_for_anova) # not significant
 invsimp_moss.anova<-aov(InvSimpson ~ MossAssociated, alpha_for_anova) # not sig
 
-shannon_layer.anova<-aov(Shannon ~ CrustLayer, alpha_for_anova) #not sig
-invsimp_layer.anova<-aov(InvSimpson ~ CrustLayer, alpha_for_anova) # not significant
+shannon_layer.anova<-aov(Shannon ~ CrustLayer, alpha_for_anova) #p=5.79e-01
+invsimp_layer.anova<-aov(InvSimpson ~ CrustLayer, alpha_for_anova) # p=0.00469
 
 pdf("~/Dropbox/GitHub/DesertCrust/alphadiversity_crustcat.pdf", useDingbats = F)
 p = plot_richness(physeq.f, x = "CrustCategory",  measures = c("Shannon", "InvSimpson"))
@@ -243,8 +243,8 @@ p + xlab("") + geom_boxplot()
 dev.off()
 
 #posthoc tests for crust category only b/c it was the only one of the three that was sig
-tukey.shannon.crust<-TukeyHSD(shannon_crust.anova) #hypol-biological were sig different. p=0.0044981
-tukey.InvSimpson.crust<-TukeyHSD(invsimp_crust.anova)#hypol-biological were sig different. p = 0.0053616
+tukey.shannon.crust<-TukeyHSD(shannon_crust.anova) #not significan
+tukey.InvSimpson.crust<-TukeyHSD(invsimp_crust.anova)#hypol-biological were sig different. p = 0.0073955
 
 
 
